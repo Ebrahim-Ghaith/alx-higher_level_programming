c5bbe7a158285e1d0eb673e494e1d9941740a625
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-10 01:39:04 +0300

    test

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-x = 97
-while x <= 122:
-    if x == 101 or x == 113:
-        print(end="")
-    else:
-        char = chr(x)
-        print("{}".format(char), end="")
-    x += 1
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end="")
