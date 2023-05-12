6868d12bff6dc47657586623f4af525dc0747e49
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-12 15:33:53 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n:
         if n == 1:
-            print("{} argument:".format(n))
+            print("1 argument:")
         else:
             print("{} arguments:".format(n))
         while x < n:
