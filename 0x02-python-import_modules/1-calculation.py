88bc567e4cac13f49e35f146af3e7314b51e39cb
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-12 14:24:03 +0300

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-if __name__ == "__main__":
 
     result_add = add(a, b)
     result_sub = sub(a, b)
