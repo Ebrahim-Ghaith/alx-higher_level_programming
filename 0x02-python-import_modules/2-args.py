ca33d9eb8e5e37d3d79edaf09f68fa2d1ce5d945
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-12 16:00:25 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         arguments_count,
         "argument" if arguments_count == 1 else "arguments",
         ":" if arguments_count > 0 else ".",
-        "\n" if arguments_count > 0 else ""
+        "" if arguments_count > 0 else ""
         ))
 
     for i, arg in enumerate(arguments, start=1):
