b0bf11b958be0a97fbc3ad432ef67acd87f1db81
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-12 14:19:05 +0300

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,13 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(a, "+", b, "=", add(a, b))
-    print(a, "-", b, "=", sub(a, b))
-    print(a, "*", b, "=", mul(a, b))
-    print(a, "/", b, "=", div(a, b))
+
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print(a, "+", b, "=", result_add)
+    print(a, "-", b, "=", result_sub)
+    print(a, "*", b, "=", result_mul)
+    print(a, "/", b, "=", result_div)
