f8b90494d719c03a9521d15e49ae25ea318227bf
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-12 15:32:47 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n:
         if n == 1:
-            print("{} argument".format(n))
+            print("{} argument:".format(n))
         else:
-            print("{} arguments".format(n))
+            print("{} arguments:".format(n))
         while x < n:
             print("{}: {}".format(n, sys.argv[x + 1]))
             x += 1
     else:
-        print("0 arguments")
+        print("0 arguments.")
