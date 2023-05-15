c2bd1a40e47ae6720946ae5585d952aebfa0e4a2
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-16 02:27:19 +0300

    test

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    x = len(my_list) - 1
+    if idx < 0:
+        return mu_list
+    elif idx > x:
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
