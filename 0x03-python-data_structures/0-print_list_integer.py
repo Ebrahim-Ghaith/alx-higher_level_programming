4de629d570c65b40f9650472dbfedce489898e41
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-05-16 01:36:32 +0300

    test

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+    x = 0
+    y = len(my_list)
+    while x < y:
+        print("{}".format(my_list[x]))
+        x += 1
+
--- 0x03-python-data_structures/0-print_list_integer1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    x = 0
-    y = len(my_list)
-    while x < y:
-        print("{}".format(my_list[x]))
-        x += 1
-
