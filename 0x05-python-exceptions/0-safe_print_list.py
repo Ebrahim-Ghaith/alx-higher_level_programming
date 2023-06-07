ce7b36903522dc09a35b7781c61e64d5970a9023
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-06-07 20:05:21 +0300

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print('{}'.format(my_list[i]), end='')
                 y += 1
-            except:
+            except ValueError as e:
                 continue
     print()
     return y
