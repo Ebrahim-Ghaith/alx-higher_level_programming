4c519cd651ed5f9411b6bd650f1b79ba83075946
Ebrahim-Ghaith <84124169+Ebrahim-Ghaith@users.noreply.github.com>
2023-11-24 20:06:34 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
 
-import MySQLdb
-from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == '__main__':
+Base = declarative_base()
+
+
+class State(Base):
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
     """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    __tablename__ = 'states'
 
-    db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states")
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
